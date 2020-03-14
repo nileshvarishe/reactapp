059d80a77fea64353a5e4b41eafb87cd435f89c0
Nilesh Varishe <nilesh.varishe@etouches.com>
2020-03-14 12:48:02 +0530

    changes in app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Nilesh <code>src/App.js</code> and save to reload.
+          Nilesh Varishe first commit git <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
