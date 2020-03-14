60b0558299b13e84719c025db8dc2d1a0dd52263
Nilesh Varishe <nilesh.varishe@etouches.com>
2020-03-14 12:55:54 +0530

    branch-1 changes in app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Nilesh Varishe first commit git <code>src/App.js</code> and save to reload.
+          branch-1 Nilesh Varishe first commit git <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
